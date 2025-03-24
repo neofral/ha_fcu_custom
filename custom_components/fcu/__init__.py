6da19aeeffbff9519f19ec9b51014a1806ff678e
neofral <neofral@users.noreply.github.com>
2025-03-24 22:19:58 +0000

    challening first error

--- custom_components/fcu/__init__.py
+++ custom_components/fcu/__init__.py
@@ -1,9 +1,7 @@
 """Initialize the ha_fcu_custom integration."""
 import logging
-import homeassistant.helpers.config_validation as cv
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.const import CONF_IP_ADDRESS, CONF_NAME
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -13,7 +11,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up the integration from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    hass.config_entries.async_setup_platforms(entry, ["climate"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["climate"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
