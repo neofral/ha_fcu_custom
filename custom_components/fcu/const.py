8fd4eadcf8596c67d6904baa7586d9d31bddc857
neofral <neofral@users.noreply.github.com>
2025-03-25 15:07:35 +0000

    final 2.3

--- custom_components/fcu/const.py
+++ custom_components/fcu/const.py
@@ -1,5 +1,5 @@
 """Constants for the FCU integration."""
 from homeassistant.const import CONF_NAME, CONF_IP_ADDRESS
 
-DOMAIN = "fcu_integration"
+DOMAIN = "fcu"
 TOKEN_UPDATE_INTERVAL = 180  # Seconds (3 minutes)
