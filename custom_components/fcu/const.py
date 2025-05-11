ff2f1c5cc44271533352636554605507ab4e9bee
neofral <neofral@users.noreply.github.com>
2025-05-11 11:23:46 +0200

    fixing update interval

--- custom_components/fcu/const.py
+++ custom_components/fcu/const.py
@@ -5,11 +5,7 @@ from datetime import timedelta
 
 DOMAIN = "fcu"
 TOKEN_UPDATE_INTERVAL = 180  # Seconds (3 minutes)
-<<<<<<< HEAD
 SCAN_INTERVAL = timedelta(seconds=30)  # Update every 30 seconds
-=======
-SCAN_INTERVAL = timedelta(seconds=30)  # Update interval for the climate entity
->>>>>>> d50f2c95bd5a4445f837654d93ba8c88fbed760c
 PLATFORMS = [Platform.CLIMATE, Platform.SENSOR]
 
 # Add sensor constants
