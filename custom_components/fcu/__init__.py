3f60120b06bd5bf90106106bdf482714d2f774c5
neofral <neofral@users.noreply.github.com>
2025-05-20 17:26:51 +0200

    fixing errors

--- custom_components/fcu/__init__.py
+++ custom_components/fcu/__init__.py
@@ -1,21 +1,24 @@
+"""Fan Coil Unit integration."""
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, PLATFORMS
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up from a config entry."""
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the FCU component."""
     hass.data.setdefault(DOMAIN, {})
-    # Create a dict to store entities for this entry
-    hass.data[DOMAIN][entry.entry_id] = {}
+    return True
 
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up FCU from a config entry."""
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    entry.async_on_unload(entry.add_update_listener(update_listener))
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
-    return unload_ok
+async def update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
